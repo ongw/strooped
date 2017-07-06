9e9e004ab264a47fc18a660552075a6e83f66fc5
Wes Jefferson Ong <wesjong@gmail.com>
2017-07-06 14:47:26 -0700

    Implement color tile swapping

--- Strooped/ColorTile.swift
+++ Strooped/ColorTile.swift
@@ -7,3 +7,129 @@
 //
 
 import Foundation
+import SpriteKit
+
+/* Tracking enum for color values */
+enum Color: String {
+    case red = "red",
+    blue = "blue",
+    yellow = "yellow",
+    green = "green",
+    pink = "pink",
+    orange = "orange",
+    black = "black",
+    purple = "purple",
+    null = ""
+}
+
+class ColorTile: SKSpriteNode {
+    
+    var textColor: Color!
+    var textValue: Color!
+    
+    var frontLabel: SKLabelNode = SKLabelNode()
+    
+    /* Bool for whether ColorTile is main */
+    var isMain: Bool = false
+    
+    /* Static variable to track user touch input */
+    static var selectedColor: Color = .null
+    
+    init(textColor: Color, textValue: Color, isMain: Bool = false) {
+        
+        /* Initialize tile texture and label */
+        var texture: SKTexture
+        
+        /* Check if tile is main tile */
+        if isMain {
+            texture = SKTexture(imageNamed: "mainTile2-1")
+            frontLabel.fontSize = 22
+        }
+        else {
+            texture = SKTexture(imageNamed: "commonTile3")
+            frontLabel.fontSize = 22
+        }
+        
+        /* Label font formatting */
+        frontLabel.zPosition = 1
+        frontLabel.verticalAlignmentMode = SKLabelVerticalAlignmentMode.center
+        frontLabel.fontName = "Ge Body"
+
+        /* Required init for implementation */
+        super.init(texture: texture, color: UIColor.clear, size: texture.size())
+        
+        /* Set up Tile Values */
+        self.setValue(textColor: textColor, textValue: textValue, isMain: isMain)
+        
+        /* Add label to tile */
+        self.addChild(frontLabel)
+        
+        /* Set up anchor point to center of tile */
+        self.anchorPoint = CGPoint(x:0.5, y:0.5)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        /* Called when a touch begins */
+        if !isMain {
+            
+        /* Update selectedColor variable */
+        ColorTile.selectedColor = self.textValue
+        }
+        else {
+            ColorTile.selectedColor = .null
+        }
+    }
+    
+    func setValue(textColor: Color, textValue: Color, isMain: Bool = false) {
+        /* Initialize tile variables */
+        self.textValue = textValue
+        self.textColor = textColor
+        self.isMain = isMain
+        
+        /* Set text of label */
+        self.frontLabel.text = textValue.rawValue
+        
+        /* Check if tile is main tile */
+        if isMain {
+            texture = SKTexture(imageNamed: "mainTile2-1")
+            frontLabel.fontSize = 22
+        }
+        else {
+            texture = SKTexture(imageNamed: "commonTile3")
+            frontLabel.fontSize = 22
+        }
+        
+        /* Label text attributes */
+        frontLabel.fontColor = ColorTile.getColor(textColor)
+    }
+    
+    /* You are required to implement this for your subclass to work */
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+    }
+    
+    static func getColor(_ color: Color) -> UIColor{
+        /* Returns UIColor for TextColor enum */
+        switch color {
+        case .red:
+            return UIColor.red
+        case .blue:
+            return UIColor.blue
+        case .green:
+            return UIColor(red: 0.153, green: 0.427, blue: 0.212, alpha: 1)
+        case .black:
+            return UIColor.black
+        case .pink:
+            return UIColor(red: 0.9137, green: 0.5686, blue: 0.7412, alpha: 1)
+        case .orange:
+            return UIColor.orange
+        case .purple:
+            return UIColor.purple
+        case .yellow:
+            return UIColor(red: 0.922, green: 0.847, blue: 0.208, alpha: 1)
+        default:
+            return UIColor.cyan
+        }
+    }
+}
--- Strooped/TileGrid.swift
+++ Strooped/TileGrid.swift
@@ -7,3 +7,145 @@
 //
 
 import Foundation
+import SpriteKit
+
+extension MutableCollection where Indices.Iterator.Element == Index {
+    /// Shuffles the contents of this collection.
+    mutating func shuffle() {
+        let c = count
+        guard c > 1 else { return }
+        
+        for (firstUnshuffled , unshuffledCount) in zip(indices, stride(from: c, to: 1, by: -1)) {
+            let d: IndexDistance = numericCast(arc4random_uniform(numericCast(unshuffledCount)))
+            guard d != 0 else { continue }
+            let i = index(firstUnshuffled, offsetBy: d)
+            swap(&self[firstUnshuffled], &self[i])
+        }
+    }
+}
+
+extension Sequence {
+    /// Returns an array with the contents of this sequence, shuffled.
+    func shuffled() -> [Iterator.Element] {
+        var result = Array(self)
+        result.shuffle()
+        return result
+    }
+}
+
+class TileGrid: SKSpriteNode {
+    
+    /* 3x3 grid of ColorTiles */
+    var tileArray = [[ColorTile]]()
+    
+    /* Tracker for main tile */
+    var mainTile: ColorTile!
+    
+    /* Array of colors for various function usage */
+    static var color: [Color] = [.null, .red, .null, .blue, .green, .null, .yellow, .null]
+    
+    init() {
+        /* Populate the grid with tiles */
+        super.init(texture: SKTexture.init(), color: UIColor.clear, size: CGSize(width: 0, height: 0))
+        
+        /* Initialize text labels */
+        var colorCount = 0
+        
+        /* Loop through columns */
+        for gridX in 0..<3 {
+            
+            /* Initialize empty column */
+            tileArray.append([])
+            
+            /* Loop through rows */
+            for gridY in 0..<3 {
+                
+                /* Create a new tile at row / column array position */
+                var newTile: ColorTile
+                
+                /* If center tile, define as main */
+                if gridX == 1 && gridY == 1 {
+                    newTile = ColorTile(textColor: .null, textValue: .null, isMain: true)
+                    mainTile = newTile
+                    changeMainTile()
+                }
+                else {
+                    newTile = ColorTile(textColor: .black, textValue: TileGrid.color[colorCount])
+                    colorCount += 1
+                }
+                
+                /* Set tile position */
+                newTile.position = CGPoint(x: Double(gridX*90) + 12.5*Double(gridX + 1) + 45, y: Double(gridY*90) + 12.5*Double(gridY + 1) + 95)
+                
+                /* Add new tile as child node*/
+                addChild(newTile)
+                
+                /* Append new tile to array */
+                tileArray[gridX].append(newTile)
+            }
+        }
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        /* Called when a touch begins */
+        
+        
+        if ColorTile.selectedColor != .null {
+        /* Check for wrong selection */
+        checkTouch()
+        
+        /* Change main tile */
+        changeMainTile()
+            
+        /* Change normal tiles */
+        randomizeNormalTiles()
+        }
+    }
+    
+    func checkTouch(){
+        /* Check if tapped node matches text color of main node */
+        if ColorTile.selectedColor != .null {
+            if ColorTile.selectedColor == mainTile.textColor {
+                GameScene.gameState = .ready
+            }
+            else {
+                GameScene.gameState = .gameOver
+            }
+        }
+    }
+    
+    func changeMainTile() {
+        /* Set up array of selections */
+        var mainTileColorArray = [Color]()
+        
+        /* Get all current non-null colors */
+        for color in TileGrid.color {
+            if color != .null {
+                mainTileColorArray.append(color)
+            }
+        }
+        
+        /* Randomly set color text and value */
+        mainTile.setValue(textColor: mainTileColorArray[Int(arc4random_uniform(UInt32(mainTileColorArray.count)))], textValue: mainTileColorArray[Int(arc4random_uniform(UInt32(mainTileColorArray.count)))], isMain: true)
+    }
+    
+    func randomizeNormalTiles() {
+        /* Shuffle colors */
+        TileGrid.color.shuffle()
+        
+        var colorCount = 0
+        for x in 0..<3 {
+            for y in 0..<3 {
+               if tileArray[x][y] != mainTile {
+                    tileArray[x][y].setValue(textColor: .black, textValue: TileGrid.color[colorCount])
+                    colorCount += 1
+                }
+            }
+        }
+    }
+    
+    /* You are required to implement this for your subclass to work */
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+}
